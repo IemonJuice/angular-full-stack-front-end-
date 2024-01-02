d6c0bacd4b0d61c073b70dec6415d15f4c729130
Yurchenko Pavlo <yurchenkopavlo3@gmail.com>
2024-01-02 20:30:21 +0200

    made profile page

--- src/app/features/profile/components/profile/profile.component.ts
+++ src/app/features/profile/components/profile/profile.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {AuthState} from "../../../../store/models/state";
+import {Store} from "@ngrx/store";
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  username: string = '';
+  character: string = '';
+  email: string = '';
+  store: Store<{ auth: AuthState }> = inject(Store<{ auth: AuthState }>)
 
+  ngOnInit() {
+    this.store.select('auth').subscribe(user => {
+      if ("email" in user.user && "username" in user.user && "character" in user.user) {
+        this.email = user.user.email
+        this.username = user.user.username
+        this.character = user.user.character;
+      }
+    })
+  }
 }
